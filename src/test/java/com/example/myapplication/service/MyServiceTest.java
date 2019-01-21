69885b443b1387b667160f6575feb637b4f2630a
Igor Moiseyenko <imoiseyenko93@gmail.com>
2019-01-21 17:06:08 +0100

    MyService unit tests update.

--- src/test/java/com/example/myapplication/service/MyServiceTest.java
+++ src/test/java/com/example/myapplication/service/MyServiceTest.java
@@ -1,9 +1,12 @@
 package com.example.myapplication.service;
 
+import com.example.myapplication.DBConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,6 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(locations = "classpath:config/application-test.properties")
+@Import(DBConfig.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MyServiceTest {
 
     @Autowired
