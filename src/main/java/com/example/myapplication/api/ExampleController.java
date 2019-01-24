2c905e538173485e6e67aa3c74c2e8455158088f
Igor Moiseyenko <imoiseyenko93@gmail.com>
2019-01-24 15:13:03 +0100

    Swagger API documentation & UI for all profiles except production.

--- src/main/java/com/example/myapplication/api/ExampleController.java
+++ src/main/java/com/example/myapplication/api/ExampleController.java
@@ -4,7 +4,7 @@ import com.example.myapplication.service.MyService;
 import com.example.myapplication.aop.ServiceException;
 import com.example.myapplication.service.dto.ItemDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -16,27 +16,27 @@ public class ExampleController {
     @Autowired
     private MyService myService;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     String home() {
         return myService.getGreeting();
     }
 
-    @RequestMapping("/items")
+    @GetMapping("/items")
     List<ItemDTO> items() throws ServiceException {
         return myService.getItemsByIds(Arrays.asList(1, 2));
     }
 
-    @RequestMapping("/all-items")
+    @GetMapping("/all-items")
     List<ItemDTO> allItems() throws ServiceException {
         return myService.getAllItems();
     }
 
-    @RequestMapping("/service-ex")
+    @GetMapping("/service-ex")
     String serviceEx() throws ServiceException {
         return myService.raiseServiceEx();
     }
 
-    @RequestMapping("/ex")
+    @GetMapping("/ex")
     String ex() throws Exception {
         return myService.raiseEx();
     }
